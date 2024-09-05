dfd4783de5f3e9752f3f5e35dd109cdeeba23e9f
Justin Mach <bachmach24k@gmail.com>
2024-09-05 12:04:40 -0500

    add: model and btn resources

--- app/src/main/java/com/example/kt_caclulator_app/model/CalculatorModel.kt
+++ app/src/main/java/com/example/kt_caclulator_app/model/CalculatorModel.kt
@@ -1,8 +1,36 @@
 package com.example.kt_caclulator_app.model
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class CalculatorModel : ViewModel() {
-    var result = 0.0
+    private val LOG_TAG = "CalculatorModel"
+    private var result : Double = 0.0
+    private var prevResult : Double = 0.0
     var operationStack = mutableListOf(Int)
+
+    fun calculate(num1: Int, num2: Int, operator: Char): Double {
+        prevResult = result
+        Log.d(LOG_TAG, "$num1 $operator $num2")
+        when (operator) {
+            '+' -> result = num1.toDouble() + num2.toDouble()
+            '-' -> result = num1.toDouble() - num2.toDouble()
+            '*' -> result = num1.toDouble() * num2.toDouble()
+            '/' -> result = num1.toDouble() / num2.toDouble()
+        }
+        Log.d(LOG_TAG, "Result: $result")
+        return result
+    }
+
+    fun clear() {
+        result = 0.0
+        prevResult = 0.0
+        operationStack.clear()
+    }
+
+    fun undo() {
+        if (operationStack.isNotEmpty()) {
+            operationStack.removeAt(operationStack.size - 1)
+        }
+    }
 }
